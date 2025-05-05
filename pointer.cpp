b8c0a48baeff8e8454ae1b19243c11e1cdf5c3a9
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-05-05 19:19:12 +0700

    first commit

---
+++ pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // dereference
+    int a = 10;
+    int b = 6;
+    int y = 20;
+    int *c = &a;
+    cout << "Alamat memori : " << c << endl;
+    cout << "Nilai dari c : " << *c << endl;
+    c = &y;
+    cout << "Alamat memori : " << c << endl;
+    cout << " Nilai dari c : " << *c << endl;
+
+    // reference
+    int &d = b;
+    cout << "Nilai " << d << endl;
+    cout  << "reference " << &d << endl;
+
+    int f = 5;
+    int g = f;
+    
+
+}
